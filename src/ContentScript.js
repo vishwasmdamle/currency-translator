128ea8bd6703641fd0bc6f868af9f366e73aa232
Vishwas Damle <vishwasmdamle@gmail.com>
2015-10-09 12:21:01 +0530

    Pulled out popup building logic.

--- src/ContentScript.js
+++ src/ContentScript.js
@@ -60,83 +60,16 @@ var Translator = function() {
         return text;
     }
 
-    var bindHover = function(currencyData) {
+    var bindHover = function() {
         $(".currency-tag").hover(
             function() {
-                displayPopup(currencyData, this)
+                var symbol = this.innerText.match(self.currencySymbols);
+                var amount = numberConverter.getNumericalAmount(this.innerText);
+                new PopupHandler(numberConverter, self.currencyData).displayPopup(symbol, amount);
             },
             function() {
                 $('#conversion-popup').hide();
             }
         );
-    }
-
-    var displayPopup = function(currencyData, currencyTag) {
-        var symbol = currencyTag.innerText.match(self.currencySymbols);
-        var currency;
-        $.each(currencyData.currencyMetadata, function(key, value) {
-            if(value.currencySymbol == symbol) {currency = value;}
-        });
-
-        var popupElement;
-        if($('#conversion-popup').length != 0) {
-            popupElement = $('#conversion-popup');
-            $('#conversion-popup').show();
-        } else {
-            popupElement = $("<div id='conversion-popup'></div>");
-        }
-
-        buildPopup(currencyData, currency, popupElement, currencyTag);
-
-    }
-
-    var buildPopup = function(currencyData, currency, element, currencyTag) {
-        var primaryConversionRate = currencyData.currencyMetadata[currency.id].conversion;
-        var amount = numberConverter.getNumericalAmount(currencyTag.innerText);
-
-        var tr, td, p, img, div, className;
-        var convertedAmount, formattedAmount;
-        div = document.createElement('div');
-        img = document.createElement('img');
-        img.src = chrome.extension.getURL("/images/AegonGold.png");
-        div.appendChild(img);
-
-        convertedAmount = (amount / primaryConversionRate).toFixed(2);
-        formattedAmount = numberConverter.getFormattedAmount(convertedAmount);
-        p = document.createElement('p');
-        p.innerHTML = currencyData.currencyMetadata[currencyData.hostCurrency].currencySymbol
-            + ' ' + formattedAmount;
-        className = 'conversion-popup ' + getSizeClass(formattedAmount.length);
-
-        div.appendChild(p);
-
-        div.appendChild(document.createElement('hr'));
-
-        for(var index in currencyData.currencyMetadata) {
-            if(currencyData.currencyMetadata[index].id != currencyData.hostCurrency) {
-                convertedAmount = (amount / primaryConversionRate * currencyData.currencyMetadata[index].conversion).toFixed(2);
-                p = document.createElement('p');
-                p.innerHTML = currencyData.currencyMetadata[index].currencySymbol
-                    + ' ' + numberConverter.getFormattedAmount(convertedAmount);
-                div.appendChild(p);
-            }
-        }
-
-        element.empty();
-        element.append(div);
-        element.removeClass();
-        element.addClass(className);
-        $(document.body).append(element);
-    };
-
-    var getSizeClass = function(length) {
-        switch (true) {
-            case (length <= 10):
-                return "small";
-            case (length > 10 && length <= 15):
-                return "medium";
-            default:
-                return "large";
-        }
     }
 }
---
+++ src/PopupHandler.js
@@ -0,0 +1,79 @@
+
+var PopupHandler = function(numberConverter, currencyData) {
+    var self = this;
+
+    this.displayPopup = function(symbol, amount) {
+        self.amount = amount;
+        setupPrimaryConversionRate(symbol);
+
+        var popupElement;
+        if($('#conversion-popup').length != 0) {
+            popupElement = $('#conversion-popup');
+            $('#conversion-popup').show();
+        } else {
+            popupElement = $("<div id='conversion-popup'></div>");
+        }
+
+        popupElement.empty();
+        popupElement.append(buildPopup());
+        setupPopupWidth(popupElement);
+        $(document.body).append(popupElement);
+    }
+
+    var buildPopup = function() {
+        var div = document.createElement('div');
+        addIcon(div);
+        appendParagraphElement(div,
+            currencyData.currencyMetadata[currencyData.hostCurrency].currencySymbol,
+            (self.amount / self.primaryConversionRate).toFixed(2));
+        div.appendChild(document.createElement('hr'));
+        appendOtherCurrencies(div);
+        return div;
+    };
+
+    var addIcon = function(div) {
+        var img = document.createElement('img');
+        img.src = chrome.extension.getURL("/images/AegonGold.png");
+        div.appendChild(img);
+    }
+
+    var appendOtherCurrencies = function(div) {
+        $.each(currencyData.currencyMetadata, function(key, value) {
+            if(value.id != currencyData.hostCurrency) {
+                convertedAmount = (self.amount / self.primaryConversionRate * value.conversion).toFixed(2);
+                appendParagraphElement(div, value.currencySymbol, convertedAmount)
+            }
+        });
+    }
+
+    var appendParagraphElement = function(div, currencySymbol, convertedAmount) {
+        p = document.createElement('p');
+        p.innerHTML = currencySymbol + ' ' + numberConverter.getFormattedAmount(convertedAmount);
+        div.appendChild(p);
+    }
+
+    var setupPopupWidth = function(element, p) {
+        var className = 'conversion-popup ' + getSizeClass(element.find('p')[0].innerText.length);
+        element.removeClass();
+        element.addClass(className);
+    }
+
+    var getSizeClass = function(length) {
+        switch (true) {
+            case (length <= 10):
+                return "small";
+            case (length > 10 && length <= 15):
+                return "medium";
+            default:
+                return "large";
+        }
+    }
+
+    var setupPrimaryConversionRate = function(symbol) {
+        var currency;
+        $.each(currencyData.currencyMetadata, function(key, value) {
+            if(value.currencySymbol == symbol) {currency = value;}
+        });
+        self.primaryConversionRate = currencyData.currencyMetadata[currency.id].conversion;
+    }
+}
